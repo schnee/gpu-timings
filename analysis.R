9241c39a15a81e627ff1488a596cb21326c3ce96
Brent Schneeman <schneeman@gmail.com>
2017-12-12 13:04:18 -0600

    labs

--- analysis.R
+++ analysis.R
@@ -9,5 +9,6 @@ timing <- read_csv("./timings.csv")
 ggplot(timing, aes(x=model, y=seconds, color=gpu)) + geom_boxplot() +
   theme_few() +
   theme(axis.text.x = element_text(angle=45, hjust=1)) +
-  ggtitle("Keras Examples Training Times") 
+  ggtitle("Keras Examples Training Times") + ylab("Seconds per Epoch") +
+  xlab("Example Name")
        
